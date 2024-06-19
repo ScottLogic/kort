acc73baa76ff2d74c9cfa1549fc5ccf12bf98b31
kclark <kclark@scottlogic.com>
2024-06-19 08:48:48 +0100

    Adding in tests for questions/answers and node tree

--- playwright/pages/editTreeTestPage.js
+++ playwright/pages/editTreeTestPage.js
@@ -3,6 +3,7 @@ export class EditTreeTestPage {
     /**
     * @param {import('@playwright/test').Page} page
     */
+
     constructor(page) {
         this.page = page;
         this.header = this.page.locator('h1', { hasText: 'Edit Tree Test' });
@@ -14,13 +15,15 @@ export class EditTreeTestPage {
         this.notAcceptingResponses = this.page.locator('radio', { hasText: 'Not accepting Responses' });
         this.submitButton = page.locator('#submitBtn');
         this.cancelButton = page.getByRole('button', { name: 'Cancel' });
-        this.taskTable = page.locator('#tasks')
-    
+        this.taskTable = page.locator('#tasks');
+        this.addChildNode = page.locator('#createNewNode');
+        this.addRootNode = page.locator('#createNewRoot');
+        this.expandTableNodes = page.locator('#expandAll');
+        this.nodeTree = page.locator('#tree')
     }
 
     async fillLastRow(questionString) {
         await this.newTaskButton.click();
-        this.taskTable = this.page.locator('#tasks');
         this.emptyRow = await this.taskTable.locator('textarea').last();
         this.emptyRow.fill(questionString);
     }
@@ -40,26 +43,20 @@ export class EditTreeTestPage {
         await this.row.locator('a:has-text("Remove")').click();
     }
 
-    /*
-    * The issue in here is the way kort saves the text. Because we have entered the 
-    * string previously, it does not appear in the DOM until the edited test is saved.
-    * 
-    * Therefore we still need to find the last row and navigate from there which I have left 
-    * in the code below.
-    * 
-    * An alternative could be to save the test and come back into it, however I 
-    * don't like that as we are just needlessly hitting more pages.
-    * 
-    * Have a play and see what you think - this works, but it's not clean.
-    *
-    * */
     async addAnswer(rowQuestion) {
-        //Same as you did above to get the text area, however at the end
-        //I have locator('../..') which acts the same as it does in CMD, just navigates
-        //back two levels. From there, I now have the tr element for the last row.
         this.emptyRow = await this.taskTable.locator('textarea').last().locator('../..');
-
-        //Now that I have the required row, I can just locate with the button ID and click
         await this.emptyRow.locator('#newAnswer').click();
+    }
+
+    async answerText(rowQuestion) {
+        this.emptyRow = await this.taskTable.locator('textarea').last().locator('../..');
+        this.newAnswer = this.emptyRow.locator('p:has-text("false")');
+    }
+
+    async expandTableAndAddNodes() {
+        this.expandTableNodes.click();
+        this.addRootNode.click();
+        this.nodeTree.locator('a:has-text("Fruits")').click();
+        this.addChildNode.click();
     }
 }
--- playwright/tests/editTreeTests.spec.js
+++ playwright/tests/editTreeTests.spec.js
@@ -1,13 +1,14 @@
 const { test, expect} = require('../fixtures/pageFixtures');
+const { EditTreeTestPage } = require('../pages/editTreeTestPage');
 
 test.beforeEach('Navigation to studies', async ({loginPage, studiesPage}) => {
     await loginPage.goto();
     await loginPage.login();
     await studiesPage.goto();
-    // await studiesPage.clickNewStudy();
 });
 
 // test('Edit tree test title and confirm changes are saved', async ({studiesPage, editTreeTestPage}) => {
+//     await studiesPage.clickNewStudy();
 //     await studiesPage.clickEditButtonForFirstTableRow();
 //     await expect(editTreeTestPage.header).toContainText('Edit Tree Test');
 //     await editTreeTestPage.editTestTitle.clear();
@@ -38,9 +39,20 @@ test.beforeEach('Navigation to studies', async ({loginPage, studiesPage}) => {
 //     await expect(studiesPage.namedRow('Changed Title 3')).toContainText('Accepting Responses');
 // });
 
-    test('Find LastRow', async ({studiesPage, editTreeTestPage}) => {
-        await studiesPage.clickEditButtonForFirstTableRow();
-        await expect(editTreeTestPage.header).toContainText('Edit Tree Test');
-        await editTreeTestPage.fillLastRow('I am a string');
-        await editTreeTestPage.addAnswer('I am a string');
-    });
+// test('Find LastRow', async ({studiesPage, editTreeTestPage}) => {
+//     await studiesPage.clickEditButtonForFirstTableRow();
+//     await expect(editTreeTestPage.header).toContainText('Edit Tree Test');
+//     await editTreeTestPage.fillLastRow('I am a string');
+//     await editTreeTestPage.addAnswer('I am a string');
+//     await editTreeTestPage.answerText('I am a string')
+//     await expect(editTreeTestPage.newAnswer).toContainText('false');
+//     await editTreeTestPage.submitButton.click();
+// });
+
+test('Confirm tree table adds nodes', async ({studiesPage, editTreeTestPage}) => {
+    await studiesPage.clickEditButtonForFirstTableRow();
+    await expect(editTreeTestPage.header).toContainText('Edit Tree Test');
+    await editTreeTestPage.expandTableAndAddNodes();
+    // await expect(editTreeTestPage.nodeTree).toContainText('New Item');
+    // await expect(editTreeTestPage.nodeTree).toContainText('New Root Item');
+});
