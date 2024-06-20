c22451f90255787fc7cf5a2c726b2a178515f5f6
kclark <kclark@scottlogic.com>
2024-06-20 13:07:22 +0100

    Preview page test

--- playwright/pages/previewPage.js
+++ playwright/pages/previewPage.js
@@ -3,7 +3,29 @@ export class PreviewPage {
     /**
     * @param {import('@playwright/test').Page} page
     */
+
     constructor(page) {
         this.page = page;
+        this.header = this.page.locator('.navbar-brand');
+        this.taskNum = this.page.locator('#taskNum');
+        this.taskQuestion = this.page.locator('#taskDesc');
+        this.nextButton = page.getByRole('button', { name: 'Next' });
+        this.finishButton = page.getByRole('button', { name: 'Finish' });
+        this.giveUpButton = page.getByRole('button', { name: 'Give Up' });
+        this.nodeTree = page.locator('#tree');
+        this.modalConfirmButton = page.getByRole('button', { name: 'Yes, Continue' });
+        
     }
+
+    async selectApple() {
+        await this.nodeTree.locator('a:has-text("Fruits")').click();
+        await this.nodeTree.locator('a:has-text("Apple")').click();
+    }
+
+    async selectBacon() {
+        await this.nodeTree.locator('a:has-text("Meats")').click();
+        await this.nodeTree.locator('a:has-text("Bacon")').click();
+    }
+
+
 }
---
+++ playwright/tests/previewTreeTests.spec.js
@@ -0,0 +1,24 @@
+const { test, expect} = require('../fixtures/pageFixtures');
+
+test.beforeEach('Navigation to studies', async ({loginPage, studiesPage}) => {
+    await loginPage.goto();
+    await loginPage.login();
+    await studiesPage.goto();
+});
+
+test('Create a basic tree and go through the preview of the tree checking that the prompts are as expected and that elements can be selected', async ({studiesPage, previewPage}) => {
+    await studiesPage.clickNewStudy();
+    await studiesPage.clickPreviewButtonForFirstTableRow();
+    await expect(previewPage.header).toContainText('Default Tree Test Title');
+    await expect(previewPage.taskNum).toContainText('Task 1 of 2');
+    await expect(previewPage.taskQuestion).toContainText('Where is the Apples?');
+    await previewPage.selectApple();
+    await previewPage.nextButton.click();
+    await previewPage.modalConfirmButton.click();
+    await expect(previewPage.taskNum).toContainText('Task 2 of 2');
+    await expect(previewPage.taskQuestion).toContainText('Where is the Bacon?');
+    await previewPage.selectBacon();
+    await previewPage.finishButton.click();
+    await previewPage.modalConfirmButton.click();
+    await expect(studiesPage.header).toContainText('Studies');
+});
