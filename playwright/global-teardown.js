40205752af13266c853085cd6331ae4f69ea041e
kclark <kclark@scottlogic.com>
2024-07-03 09:58:00 +0100

    Beginning event investigation

--- playwright/global-teardown.js
+++ playwright/global-teardown.js
@@ -5,44 +5,35 @@ const responseSchema = require('../models/responseSchema.js');
 const eventSchema = require('../models/eventSchema.js');
 
 //Here to investigate looking for event deletes
-//results: function (req, res, next) {
-// Study.findOne({_id: req.params.id, ownerID: req.user._id}, function (err, study) {
-// 	if (err) {
-// 		res.status(504);
-// 		logger.error("treetest_server.js: Error getting study to see results:", err);
-// 		res.end(err);
-// 	} else {
-// 		Response.find({_id: {$in: study.completeResponses}}, function (err, completeResponses) {
-// 			if (err) {
-// 				res.status(504);
-// 				logger.error("cardsort_server.js: Error in edit cardsort:", err);
-// 				res.end(err);
-// 			} else {
-// 				var responses = gatherResponses(study.data.tasks, completeResponses)
-// 				res.render('treetest/results.ejs',{study: study,
-// 													completeResponses:completeResponses,
-// 													email: req.user.email,
-// 													admin: req.session.admin,
-// 													taskSet: responses[0],
-// 													taskCount: responses[1]
-// 												});
-// 			}
-// 		});
 
-// function gatherResponses(tasks, responses){
-//     var ret_responses = []
-//     for (var i = 0; i < tasks.length; i++) {
-//         ret_responses.push([]);
-//     }
+function getEvents(req){
+	Study.findOne({title: req}, function (err, study) {
+		if (err) {
+			res.status(504);
+			logger.error("treetest_server.js: Error getting study to see results:", err);
+			res.end(err);
+		} else {
+			const studyId = 
+			Response.find({_id: {$in: study.completeResponses}}, function (err, completeResponses) {
+				if (err) {
+					res.status(504);
+					logger.error("cardsort_server.js: Error in edit cardsort:", err);
+					res.end(err);
+				} else {
+					var responses = gatherResponses(study.data.tasks, completeResponses)
+					res.render('treetest/results.ejs',{study: study,
+														completeResponses:completeResponses,
+														email: req.user.email,
+														admin: req.session.admin,
+														taskSet: responses[0],
+														taskCount: responses[1]
+													});
+				}
+			});
+		}
+	})
 
-//     for (var i = 0; i < responses.length; i++) {
-//         var response = responses[i]
-//         for (var j = 0; j < response.data.length; j++) {
-//             ret_responses[j].push(convertResponseArrayToString(response.data[j]));
-//         }
-//     }
-//     return countUniqueInstances(ret_responses)
-// }
+}
 
 
 /**
