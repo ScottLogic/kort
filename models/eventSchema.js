f209e29948c26fe23dc1c5e0ecfa12d0fbf4de99
kclark <kclark@scottlogic.com>
2024-07-04 14:21:06 +0100

    Getevents code

--- models/eventSchema.js
+++ models/eventSchema.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 
+var options = { discriminatorKey: 'type' };
+
 // Define the base Event schema
 const eventSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.UUID,
--- playwright/global-teardown.js
+++ playwright/global-teardown.js
@@ -4,38 +4,6 @@ const studySchema = require('../models/studySchema.js');
 const responseSchema = require('../models/responseSchema.js');
 const eventSchema = require('../models/eventSchema.js');
 
-//Here to investigate looking for event deletes
-
-function getEvents(req){
-	Study.findOne({title: req}, function (err, study) {
-		if (err) {
-			res.status(504);
-			logger.error("treetest_server.js: Error getting study to see results:", err);
-			res.end(err);
-		} else {
-			const studyId = 
-			Response.find({_id: {$in: study.completeResponses}}, function (err, completeResponses) {
-				if (err) {
-					res.status(504);
-					logger.error("cardsort_server.js: Error in edit cardsort:", err);
-					res.end(err);
-				} else {
-					var responses = gatherResponses(study.data.tasks, completeResponses)
-					res.render('treetest/results.ejs',{study: study,
-														completeResponses:completeResponses,
-														email: req.user.email,
-														admin: req.session.admin,
-														taskSet: responses[0],
-														taskCount: responses[1]
-													});
-				}
-			});
-		}
-	})
-
-}
-
-
 /**
  * Deletes all documents from the 'Study' collection.
  * @returns {Promise<Object>} - The result of the delete operation.
@@ -45,8 +13,9 @@ teardown('Clean all documents', async ({ }) => {
 		const conn = await mongoose.connect('mongodb://127.0.0.1:27017/kort');
 		console.log('deleting test database...');
 		const Study = conn.model('Study', studySchema);
-		const Response = conn.model('Response', responseSchema)
+		const Response = conn.model('Response', responseSchema);
 
+		getEvents('Response Tree Test ' , Study, Response);
 		await Response.deleteMany({ $or: [ {title: 'Name'}, {title: 'Anonymous'} ] });
 		await Study.deleteMany({ $or: [{title: 'Changed Title' }, {title: 'Tree Test Options Test' }, {title: 'Last Row Test' }, {title: 'Default Tree Test Title' }, {title: 'ResultPage Link Test' }, {title: 'Unique Link Test' }] });
 		console.log('DELETED!');
@@ -56,3 +25,17 @@ teardown('Clean all documents', async ({ }) => {
 		throw error;
 	}
 });
+
+//Gets StudyID and finds study with that id, Then gets reponseID for that studyID and THEN gets events with that responseID (currently broken as studyID is not being passed properly into the response.FindOne())
+function getEvents(studyTitle, Study, Response){
+	Study.findOne({title: studyTitle}, function (err, study) {
+
+		var studyIdVar = JSON.stringify(study._id);
+		console.log('STUDY ID: ' + studyIdVar);
+
+		Response.findOne({studyID: studyIdVar}, function (err, response) {
+			const responseIdVar = JSON.stringify(response._id)
+			console.log('RESPONSE ID: ' + responseIdVar);
+		});
+	});
+}
